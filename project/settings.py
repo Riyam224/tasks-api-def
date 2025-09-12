a6b3e4984547d22b80e6b0c7d748ad0ee9e2a42c
Riyam224 <cs50riyam211@gmail.com>
2025-09-12 23:16:59 +0300

    add allowed hosts

--- project/settings.py
+++ project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".up.railway.app"]
 
 
 # Application definition
