e172bdc4ca1331d02af0c9ff7dfdb0f8183ee7b9
Riyam224 <cs50riyam211@gmail.com>
2025-09-13 13:52:24 +0300

    edit home

--- project/urls.py
+++ project/urls.py
@@ -17,16 +17,39 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 # project/urls.py
 from django.conf import settings
 from django.conf.urls.static import static
 from tasks.views import home
 
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Tasks API 🚀",
+        default_version="v1",
+        description="A simple and modern API for managing tasks.",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="your-email@example.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 urlpatterns = [
     path("", home, name="home"),
     path("admin/", admin.site.urls),
     path("api/", include("tasks.urls")),
+    # Swagger & Redoc
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
 
 # 👇 Add this at the bottom
